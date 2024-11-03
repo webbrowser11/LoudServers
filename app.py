81c4fcca82480e361255f13370cb72834e48afdb
T_cat9000_2 <terpstragraham@gmail.com>
2024-11-02 19:25:20 -0700

    update

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ def index():
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Ping Service</title>
+        <title>Loud Servers</title>
         <style>
             body {
                 font-family: 'Arial', sans-serif;
